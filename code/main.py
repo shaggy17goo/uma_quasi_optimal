6b6765c56be60fb43215d4fc71a0a5d231c1f6ca
shaggy17goo <56591106+shaggy17goo@users.noreply.github.com>
2022-01-22 15:06:22 +0100

    final version

--- code/main.py
+++ code/main.py
@@ -16,17 +16,16 @@ if __name__ == "__main__":
     ### wygenerowanie jendego zestawu reguł i przeprowadzenie predykcji na jego podstawie (krótki czas testu)
     test_one_rule_generate()
 
-    ### Test wyboru najlepszej wartości podziału zborow trenujacego, walidacyjnego, testujacego (bardzo długi czas testu)
-    # test_diff_sets_size()
-
-    ### Test wyboru najlepszej wartości parametru best_complexes_param (bardzo długi czas testu)
-    # test_diff_best_complexes_param()
-
     ### generowanie zestawów reguł i predykcja na podstawie głosowania (długi czas testu)
     test_generate_and_test_x_rules_sets_voting(5, "test5")
 
     ### przygotowany zestawy reguł do przetestowania głosowania, wynik poprzedniego testu (krótki czas testu)
     test_rules_sets_with_voting("../data/test100/rules", "../data/test100/car.data.test")
 
+    ### Test wyboru najlepszej wartości podziału zborow trenujacego, walidacyjnego, testujacego (bardzo długi czas testu)
+    # test_diff_sets_size()
+
+    ### Test wyboru najlepszej wartości parametru best_complexes_param (bardzo długi czas testu)
+    # test_diff_best_complexes_param()
 
 
